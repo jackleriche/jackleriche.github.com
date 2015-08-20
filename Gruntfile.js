330e6e5302cad47ba3976dacc62f536ec88c8d46
Jack Le Riche <jackleriche@gmail.com>
2015-08-20 13:27:34 +0100

    setup initial grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+modue.exports = function (grunt) {
+
+	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+
+} 
