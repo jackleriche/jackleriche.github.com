1e732ab3b653458cd66e30cfcac6195edea92835
Jack Le Riche <jackleriche@gmail.com>
2015-11-04 10:26:11 +0000

    add frosty Bingo + frosty bingo wrong

---
+++ game/frosty-bingo-wrong/src/lib/EndGame.js
@@ -0,0 +1,136 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="Scene.ts" />
+/// <reference path="GameObject.ts" />
+var com;
+(function (com) {
+    var camelot;
+    (function (camelot) {
+        var iwg;
+        (function (iwg) {
+            var CAMELOT = com.camelot, CORE = CAMELOT.core, IWG = CAMELOT.iwg, GAMEOBJECT = IWG.GameObject, CLICKABLEGAMEOBJECT = IWG.ClickableGameObject, TICKET = IWG.Ticket, SPRITESHEETS = IWG.SpriteSheets;
+            var EndGame = (function (_super) {
+                __extends(EndGame, _super);
+                function EndGame(_name) {
+                    _super.call(this, _name);
+                    this._wager = CORE.IWG.ame('get', { vars: ['iwgIsWager'] });
+                    this._amount = TICKET.getInstance().getOutcome().amount;
+                    this._isWinner = Number(TICKET.getInstance().getParams().wT);
+                    this._endGameLayout();
+                    this._subscribeEndGame();
+                }
+                EndGame.prototype._subscribeEndGame = function () {
+                    IWG.IWGEM.on('showEndGame', this._showEndGame.bind(this));
+                };
+                EndGame.prototype._showEndGame = function () {
+                    TweenMax.to(this.getDiv(), 1, { scale: 1, alpha: 1, ease: Bounce.easeOut });
+                    IWG.IWGEM.trigger('disableCard');
+                };
+                EndGame.prototype._endGameLayout = function () {
+                    var endGameBackground = new GAMEOBJECT("endGameBackground", { w: 276, h: 119 }, 1, this);
+                    endGameBackground.addBitmap({
+                        name: "background",
+                        pos: {
+                            x: 138,
+                            y: 59
+                        },
+                        frame: "end_bg",
+                        spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo"),
+                        doReg: {
+                            center: true
+                        }
+                    });
+                    var endGameButton = new CLICKABLEGAMEOBJECT("endGameButton", { w: 188, h: 81 }, 1, this);
+                    endGameButton.addBitmap({
+                        name: "button",
+                        pos: {
+                            x: 94,
+                            y: 40
+                        },
+                        frame: "button_finish",
+                        spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo"),
+                        doReg: {
+                            center: true
+                        }
+                    });
+                    endGameButton.setPosition({
+                        x: 45,
+                        y: 90
+                    });
+                    endGameButton.setEnabled(true);
+                    var tl = new TimelineMax({ repeat: -1, delay: 4 });
+                    tl.add(TweenMax.to(endGameButton.getCanvas(), 1, { scaleX: 1.1, scaleY: 1.1 }), "down");
+                    tl.add(TweenMax.to(endGameButton.getCanvas(), 1, { scaleX: 1, scaleY: 1 }), "up");
+                    endGameButton.animationTimeLine = tl;
+                    endGameButton.setAction('click', function () {
+                        endGameButton.setEnabled(false);
+                        CORE.IWG.ame('closeGame');
+                    });
+                    var endTextFrame = null, endTextPos = {
+                        x: 39,
+                        y: 10
+                    };
+                    if (this._wager) {
+                        if (this._isWinner === 1) {
+                            endTextFrame = "end_congrats";
+                        }
+                        else {
+                            endTextFrame = "end_lose";
+                            endTextPos.y = 30;
+                        }
+                        ;
+                    }
+                    else {
+                        endTextFrame = "end_trial";
+                        endTextPos.y = 30;
+                    }
+                    ;
+                    var endGameText = new GAMEOBJECT("endGameText", { w: 200, h: 50 }, 1, this);
+                    endGameText.addBitmap({
+                        name: "button",
+                        pos: {
+                            x: 100,
+                            y: 25
+                        },
+                        frame: endTextFrame,
+                        spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo"),
+                        doReg: {
+                            center: true
+                        }
+                    });
+                    endGameText.setPosition({
+                        x: endTextPos.x,
+                        y: endTextPos.y
+                    });
+                    if (this._isWinner === 1 && this._wager) {
+                        var endGameAmount = new GAMEOBJECT("endGameAmount", { w: 193, h: 35 }, 1, this);
+                        endGameAmount.addBitmap({
+                            name: "button",
+                            pos: {
+                                x: 97,
+                                y: 18
+                            },
+                            frame: "end_" + this._amount,
+                            spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo"),
+                            doReg: {
+                                center: true
+                            }
+                        });
+                        endGameAmount.setPosition({
+                            x: 41,
+                            y: 60
+                        });
+                    }
+                    ;
+                };
+                return EndGame;
+            })(iwg.Scene);
+            iwg.EndGame = EndGame;
+        })(iwg = camelot.iwg || (camelot.iwg = {}));
+    })(camelot = com.camelot || (com.camelot = {}));
+})(com || (com = {}));
---
+++ game/frosty-bingo/src/lib/BallAudit.ts
@@ -0,0 +1,157 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="Scene.ts" />
+/// <reference path="GameObject.ts" />
+module com.camelot.iwg {
+
+	var CAMELOT: any 	= com.camelot,
+        CORE 			= CAMELOT.core,
+        IWG 			= CAMELOT.iwg,
+		GAMEOBJECT 		= IWG.GameObject,
+		SPRITESHEETS	= IWG.SpriteSheets;
+
+	export class BallAudit extends Scene {
+		
+		private _audit: Array<GameObject> = [];
+		private _index: number = 0;
+		
+		
+		constructor(name, private _noBalls: number = 10 ) {
+			
+			super(name);
+			this._ballAuditSubscribe();
+			
+			// setup scene
+			this.setDimensions({
+				w: 960,
+				h: 50
+			});
+			
+			this._setupLayout();	
+			
+		}
+		
+		
+		/*
+		 *	name: 			_ballAuditSubscribe
+		 *	description:	setup listeners associated with the ballAudit class
+		 *	params:			null
+		 *	returns:		null
+		 */		 
+		private _ballAuditSubscribe() {
+			IWG.IWGEM.on( 'ballAudit', this._ballAudit.bind(this) );
+		}
+		
+		/*
+		 *	name: 			_ballAuditUnsubscribe
+		 *	description:	remove listeners associated with the ballAudit class
+		 *	params:			null
+		 *	returns:		null
+		 */		 
+		private _ballAuditUnsubscribe() {
+			IWG.IWGEM.off( 'ballAudit' );
+		}
+		
+		
+		
+		
+		/*
+		 *	name: 			_setupLayout
+		 *	description: 	setup audit layout
+		 *	param:			null
+		 *	returns:		null
+		 */
+		private _setupLayout() {
+			
+			for ( var i = 0; i < this._noBalls; i++ ){
+				
+				var ball = new GAMEOBJECT("ball" + i, {w: 48, h: 48}, 1, this);
+				ball.addBitmap({
+					name: "emptyBall",
+		        	pos: {
+		            	x: 24,
+		            	y: 24
+		        	},
+		        	frame: "audit_single",
+		        	spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo") ,
+			        doReg: {
+			            center: true
+			        },
+					scale: 1
+				});
+				ball.addBitmap({
+					name: "auditBall",
+		        	pos: {
+		            	x: 24,
+		            	y: 24
+		        	},
+		        	frame: "audit_single",
+		        	spriteSheet: SPRITESHEETS.getInstance().getSpriteSheet("frostyBingo") ,
+			        doReg: {
+			            center: true
+			        },
+					scale: 0,
+					alpha: 0
+				});
+				
+				ball.setScale(0, 0);
+				ball.setAlpha(0);
+				
+				ball.setPosition({
+					x: 48 * i
+				});
+				
+				// setup shrink animation
+				ball.addAnimation("shrinkAuditBall");
+				ball.setAnimation("shrinkAuditBall", "shrinkAuditBall");
+				
+				ball.addAnimation("growAuditBall");
+				ball.setAnimation("growAuditBall", "growAuditBall");
+				
+				this.addChild(ball);
+				
+				// add to audit array for easy referencing
+				this._audit.push(ball);
+				
+				var delay = (0.5 + (i / 10));
+				
+				TweenMax.to( ball.getCanvas(), 0.3, { delay: delay, alpha: 1, scaleX: 1, scaleY: 1, ease: Bounce.easeOut } );
+				
+			}
+			
+		} // end _setupLayout
+		
+		/*
+		 *	name:			_ballAudit
+		 *	description:	event called to populate the audit
+		 *	params:			ballColor: string, ballNumber: number
+		 *	returns:		null
+		 */
+		private _ballAudit(ballColor, ballNumber) {
+			
+			// split ball to just color
+			var color = ballColor.split("_")[1];
+			
+			var auditBall = this._audit[this._index];
+			auditBall.animate("shrinkAuditBall");
+			
+			var bitmap = auditBall.getBitmap("auditBall");
+			bitmap.gotoAndStop('auditball_' + color);
+			
+			var numberWang = new createjs.Text(ballNumber, "bold 20px Arial", "black")
+			numberWang.name = 'numberWang';
+			numberWang.textAlign = "center";
+			numberWang.x = 23;
+			numberWang.y = 10;
+			numberWang.alpha = 0;
+			
+			
+			auditBall.getStage().addChild(numberWang);
+			
+			auditBall.animate("growAuditBall");
+			
+			// increse the ballAuditIndex by 1
+			this._index++;
+			
+		}
+	}
+}
