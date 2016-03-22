355774f9aa226f8b78f0586134e19cf797b8850d
Jack Le Riche <jackleriche@gmail.com>
2016-03-22 09:28:43 +0000

    changes to code

--- sites/antWalker/js/main.js
+++
@@ -1,7 +0,0 @@
-$( window ).scroll(function() {
-    if ( $(window).scrollTop() > 100 ) {
-        $( '#scrollDown').fadeOut('slow');
-    } else {
-        $( '#scrollDown').fadeIn('slow');
-    }
-})
