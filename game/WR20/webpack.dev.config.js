7d34bf7e9aea86f9f179b65d005667379770ba22
Jack Le Riche <me@jackleriche.co.uk>
2019-11-06 08:59:56 +0000

    Add files via upload
    add zdog demo

---
+++ game/WR20/webpack.dev.config.js
@@ -0,0 +1,45 @@
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: path.join(__dirname, "src/index.ts"),
+  watch: true,
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "DEV MODE: web games base",
+      template: path.join(__dirname, "templates/index.ejs")
+    }),
+    new CopyWebpackPlugin([{ from: "./assets", to: "assets" }])
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+    inline: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true,
+      ignored: /node_modules/
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
